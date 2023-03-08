81db039125310f8193242377e9314f095e388447
Luis T. Rodrigues <luisthalys1@hotmail.com>
2023-03-08 20:08:07 -0300

    Atualização do Sobre.jsx
    removendo funções de debug

--- src/pages/debug/debug.jsx
+++ src/pages/debug/debug.jsx
@@ -1,3 +1,17 @@
 // Arquivo para Criação de Funções e outras coisas para debbugar a aplicação
 
 // Por enquanto só para fazer testes no github desktop
+
+                // <div id="test">
+                //     <form>
+                //         <input type="text" placeholder='Editar Texto 1'/>                                        
+                //         <hr />
+
+                //         <input type="text" placeholder='Editar Texto 2'/>                        
+                //         <hr />
+
+                //         <button type="submit">Salvar</button>
+
+                //         <button type="reset">Resetar</button>
+                //     </form>
+                // </div>   
--- src/pages/sobre-page/Sobre.jsx
+++ src/pages/sobre-page/Sobre.jsx
@@ -24,20 +24,7 @@ function Sobre(){
                     </p>
                     
                 </div>
-
-                <div id="test">
-                    <form>
-                        <input type="text" placeholder='Editar Texto 1'/>                                        
-                        <hr />
-
-                        <input type="text" placeholder='Editar Texto 2'/>                        
-                        <hr />
-
-                        <button type="submit">Salvar</button>
-
-                        <button type="reset">Resetar</button>
-                    </form>
-                </div>                    
+                 
             </section>                    
     )
 }
