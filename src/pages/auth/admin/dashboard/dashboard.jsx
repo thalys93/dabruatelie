18f18e1403246deee06d4a8c71123232b909d6d0
Luis T. Rodrigues <luisthalys1@hotmail.com>
2023-03-14 16:28:17 -0300

    Ultimas Alterações 14.04.23
    ultimas alterações genéricas, com o footer, e outras informações.

--- src/pages/auth/admin/dashboard/dashboard.jsx
+++ src/pages/auth/admin/dashboard/dashboard.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 
 
+
 function Dashboard() {
   return (
     <div>
-      <h1> Testando o Dashboard</h1>         
+      <h1> Testando o Dashboard</h1>      
+         <h2> Usando Informações para definir alguma coisa </h2>
     </div>
   )
 }
--- src/pages/produtos-page/Produto.jsx
+++ src/pages/produtos-page/Produto.jsx
@@ -1,47 +1,41 @@
 import React, { useEffect, useState } from 'react'
-import Produto from "./Produto";
 
-
-// Import dos Estilos
+//Import dos Estilos
 import './produtos.css'
 
 
-function Produtos(){       
+function Produto() {
 
     // Buscar na Api Informações
     const [data, setData] = useState([]);
     const local = "http://26.79.95.70:1337"
 
     useEffect(() => {                            
-        fetch('http://26.79.95.70:1337/api/produtos?populate=*')
+        fetch('http://26.79.95.70:1337/api/produtos:?populate=*')
         .then((response) => response.json())
         .then(setData);
     }, []);
 
 
-    return(
-    <section id="Produtos">
-            <div className='container'>
-                <div className='carousel'>                 
-            {data.data?.map((item, id ) => {      
-            return(     
+  return (
+    <div>        
+        {data.data?.map((item, id ) => {      
+        return(     
                 <div className="item" key={id}>
                     <div className='image'> 
                         <img src={local + item.attributes.imagem.data.attributes.url }  />
                     </div>
                     <div className='info'>
                         <span className='name'>{item.attributes.titulo}</span> 
-                        <span className="categoria">({item.attributes.Categoria})</span> 
-                        <button id="modalBtn"> Saiba mais </button>                         
+                        <span className="categoria">({item.attributes.Categoria})</span>                         
+                        <span className="sobre"> {item.attributes.Sobre}</span>
                         <span className='price'>R$ {item.attributes.valor}</span>                                
                 </div>
             </div>
-            )
-            })}                    
-                </div>
-        </div>                                   
-    </section>
-    )
+        )
+        })}
+    </div>
+  )
 }
 
-export default Produtos
+export default Produto
